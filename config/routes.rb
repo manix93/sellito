cbc1b706e15cdfd7573637f9048610a5c23f3fd2
Gromo <golec.sebastian@gmail.com>
2017-04-23 12:56:40 +0200

    add apth to about

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homepage#index'
+
+  get '/about', to: 'homepage#about'
 end
