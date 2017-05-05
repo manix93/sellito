84a36707ca1d91034a634814bd9072bd5046685d
Gromo <golec.sebastian@gmail.com>
2017-05-05 20:57:42 +0200

    Add table Categories to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170423135634) do
+ActiveRecord::Schema.define(version: 20170505185307) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
