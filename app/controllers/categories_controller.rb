79c1e84e2af84fa7f6714c9b8105501816d3ab69
Gromo <golec.sebastian@gmail.com>
2017-05-06 16:12:30 +0200

    Add methods to categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,34 @@
 class CategoriesController < ApplicationController
-  def new
+  def index
+    @categories = Category.all
   end
 
-  def edit
+  def new
+    @category = Category.new
+  end
+
+  def create
+    #binding.pry # odpala pry w momencie wywołania metody
+    #raise params.to_yaml # wyrzuca wyjątek do przeglądarki
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = 'Category created!'
+      redirect_to @category
+      # odpowiednik
+      #redirect_to category_path(@category)
+    else
+      flash[:alert] = 'Could not create category'
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
