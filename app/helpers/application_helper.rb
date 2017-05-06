aea782c43c8a168b18a91e733dc5fa17a1a563f6
Gromo <golec.sebastian@gmail.com>
2017-05-06 18:44:00 +0200

    Adds few methods to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def current_user_is_admin
+    current_user && current_user.is_admin?
+  end
+
+  def login_or_logout_user_link
+    current_user ? logout_user_link : logout_user_link
+  end
+
+  def login_user_link
+    link_to 'Login', new_user_session_path
+  end
+
+  def logout_user_link
+    link_to 'Logout', destroy_user_session_path, :method => :delete
+  end
+
+  def register_user_link
+    link_to 'Register', new_user_registration_path
+  end
 end
