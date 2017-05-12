ec727eecdde61fd5da80c9249f52f67f4a3e908d
Gromo <golec.sebastian@gmail.com>
2017-05-12 19:29:59 +0200

    Add posts routes for every user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homepage#index'
+
+  resources :users do
+    resources :posts
+  end
 
   resources :categories
 
