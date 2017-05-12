810fa811898cbf3d806c1fd2c66f311473a0db18
Gromo <golec.sebastian@gmail.com>
2017-05-12 17:01:51 +0200

    Add links logic to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,15 @@
 module ApplicationHelper
+  def admin_categories_link
+    categories_link_tag = "<li>#{link_to 'Categories', categories_path}</li>"
+    categories_link_tag.html_safe if current_user_is_admin
+  end
+
   def current_user_is_admin
     current_user && current_user.is_admin?
   end
 
   def login_or_logout_user_link
-    current_user ? logout_user_link : logout_user_link
+    current_user ? logout_user_link : login_user_link
   end
 
   def login_user_link
