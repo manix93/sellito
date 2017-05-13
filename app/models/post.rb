73ca8bdee1d2b003024348dbb625520ca1fb8238
Lukasz <manix93@gmail.com>
2017-05-13 12:53:08 +0200

    Add validate post model if expiration_date_cannot_be_in_the_past

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,11 @@ class Post < ApplicationRecord
   # TODO handle managing photos
   belongs_to :user
   validates :title, :expiration_date, presence: true
+  validate :expiration_date_cannot_be_in_the_past
+
+  def expiration_date_cannot_be_in_the_past
+    if expiration_date < Date.today
+      errors.add(:expiration_date, "can't be in the past" )
+    end
+  end
 end
